486faa057bd3a48dbe9c4cf3e26ebd661caf1b2c
Zakityy <Zacharyhook24@gmail.com>
2022-05-16 00:39:17 -0700

    added stuffs

--- java.js
+++ java.js
@@ -5,5 +5,36 @@ function checker() {
     }
 }
 
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 0 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (timer-- < 0) {
+            //window.location = "http://www.example.com";
+            clearInterval(timer);
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 1 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
+
+
+
+
+
+
+
 console.log('hello if you are seeing this');
 
